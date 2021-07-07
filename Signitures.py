afad49e0741a1ee001fd9e76587af7621c3accb8
payam49er@gmail.com <payam49er@gmail.com>
2021-07-07 07:57:15 -0400

    working on transaction

--- Signitures.py
+++ Signitures.py
@@ -10,12 +10,14 @@ def generate_keys():
 
 # in a way, signing is like decrypting a message
 def sign(message,private_key):
-    signature = private_key.sign(message,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+    message = bytes(str(message),'utf-8')
+    signature = private_key.sign(message, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
                                                      salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
     return signature
 
 def verify(message, signature, public_key):
     try:
+        message = bytes(str(message),'utf-8')
         public_key.verify(signature, message, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
                                                           salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
         return True
--- Transaction.py
+++ Transaction.py
@@ -2,36 +2,66 @@ import Signitures
 
 class TX:
 
-
     def __init__(self):
-        self.inputs = []
-        self.outputs = []
-        self.sigs = []  # list of signitures
-        self.reqd = []  # list of required signitures, which are not inputs. This is for escrow
+        self.inputs = []  # holds tuple of from address and amount
+        self.outputs = []  # holds tuple of to addresses and amount
+        self.sigs = []  # list of signatures
+        self.reqd = []  # list of required signatures, which are not inputs. This is for escrow
 
     def add_input(self,from_address, amount):
-        pass
+        self.inputs.append((from_address,amount))
 
     def add_output(self, to_addr, amount):
-        pass
+        self.outputs.append((to_addr,amount))
 
     def add_reqd(self, addr):
-        pass
+        self.reqd.append(addr)
 
     def sign(self, private):
-        pass
+        message = self.__gather()
+        newsig = Signitures.sign(message, private)
+        self.sigs.append(newsig)
 
     def is_valid(self):
-        return False
+        total_input = 0
+        total_output = 0
+        for (addr, amount) in self.outputs:
+            if amount < 0:
+                return False
+            total_output += amount
+        if total_output > total_input:
+            return False
+        message = self.__gather()
+        for (addr, amount) in self.inputs:
+            total_input += amount
+            if amount < 0:
+                return False
+            for s in self.sigs:
+                found = False
+                if Signitures.verify(message, s, addr):  # message, private key, public key
+                    found = True
+                if not found:
+                    return False
+        for (addr, amount) in self.outputs:
+            if amount < 0:
+                return False
+        return True
 
 
+
+    def __gather(self):
+        data = []
+        data.append(self.inputs)
+        data.append(self.outputs)
+        data.append(self.reqd)
+        return data
 
 
 if __name__ == "__main__":
-    pr1,pu1 = Signitures.generate_keys()
-    pr2,pu2 = Signitures.generate_keys()
-    pr3,pu3 = Signitures.generate_keys()
-    pr4,pu4 = Signitures.generate_keys()
+    pr1, pu1 = Signitures.generate_keys()
+    pr2, pu2 = Signitures.generate_keys()
+    pr3, pu3 = Signitures.generate_keys()
+    pr4, pu4 = Signitures.generate_keys()
 
     tx1 = TX()
     tx1.add_input(pu1, 1)
@@ -42,5 +72,15 @@ if __name__ == "__main__":
     else:
         print("Fail, TX1 is invalid")
 
+    tx2 = TX()
+    tx2.add_input(pu2,5)
+    tx2.add_input(pu2,2)
+    tx2.add_output(pu3,5)
+    tx2.add_output(pu1,2)
+    tx2.sign(pr2)
+    if tx2.is_valid():
+        print("Success, TX2 is valid")
+    else:
+        print("Fail, TX2 is invalid")
 
 
