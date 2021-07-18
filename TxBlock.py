87633e91efd990a48e45bc85d06010926dc63865
payam shoghi <payam49er@gmail.com>
2021-07-17 23:25:39 -0400

    updated gitignore and working transaction on block

--- TxBlock.py
+++ TxBlock.py
@@ -2,16 +2,18 @@ from BasicBlockChain import CBlock
 import Signitures
 from Transaction import TX
 import pickle
+from cryptography.hazmat.primitives import serialization
 
 class TxBlock(CBlock):
-    def __init__(self, previousBlock):
-        pass
+    def __init__(self, previousBlock: CBlock):
+        self.previous_block = previousBlock
+        self.incoming_tx = []
 
     def addTx(self, Tx_in):
-        pass
+       self.incoming_tx.append(Tx_in)
 
     def is_valid(self):
-        return False
+        previous_hash = self.previous_block.previous_hash
 
 
 if __name__ == "__main__":
@@ -19,10 +21,23 @@ if __name__ == "__main__":
     pr2, pu2 = Signitures.generate_keys()
     pr3, pu3 = Signitures.generate_keys()
 
+def serialize_pr_key(private_key, message:str):
+    pm = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                              format=serialization.PrivateFormat.PKCS8,
+                              encryption_algorithm=serialization.BestAvailableEncryption(f"{bytes(message)}"))
+    return pm.splitlines()[0]
+
+
+def serialize_pu_key(public_key):
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    return pem.splitlines()[0]
+
     Tx1 = TX()
-    Tx1.add_input(pu1,3)
-    Tx1.add_output(pu2,5)
-    Tx1.sign(pr1)
+    Tx1.add_input(serialize_pu_key(pu1), 3)
+    Tx1.add_output(serialize_pu_key(pu2), 5)
+    Tx1.sign(serialize_pr_key(pr1))
     print(Tx1.is_valid())
 
     message = b"some message"
@@ -33,7 +48,7 @@ if __name__ == "__main__":
 
     savefile.close()
 
-    loadfile = open("save.dat","rb")
+    loadfile = open("save.dat", "rb")
     newTx = pickle.load(loadfile)
     print(newTx.is_valid())
 
