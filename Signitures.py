1bd711171b73e12fc3d2819dd7d55926f34e0722
payam49er@gmail.com <payam49er@gmail.com>
2021-08-11 09:11:08 -0400

    making progress toward transaction

--- Signitures.py
+++ Signitures.py
@@ -2,22 +2,37 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography import exceptions
+from cryptography.hazmat.primitives import serialization
 
+
+# returning a serialized public key and the raw private key for now
 def generate_keys():
-    private = rsa.generate_private_key(public_exponent=65537,key_size=2048)
-    public = private.public_key()
-    return private, public
+    private = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private.public_key()
+    pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                  format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    print(f"public key {pem.splitlines()[0]}")
+    return private, pem.splitlines()[0]
+
 
 # in a way, signing is like decrypting a message
-def sign(message,private_key):
-    message = bytes(str(message),'utf-8')
+def sign(message, private_key):
+    message = bytes(str(message), 'utf-8')
     signature = private_key.sign(message, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                                                     salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
+                                                      salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
     return signature
+
+def serialize_pr_key(private_key, message: str):
+        pm = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                       format=serialization.PrivateFormat.PKCS8,
+                                       encryption_algorithm=serialization.BestAvailableEncryption(bytes(message)))
+        return pm.splitlines()[0]
+
+
 
 def verify(message, signature, public_key):
     try:
-        message = bytes(str(message),'utf-8')
+        message = bytes(str(message), 'utf-8')
         public_key.verify(signature, message, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
                                                           salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
         return True
@@ -27,8 +42,9 @@ def verify(message, signature, public_key):
         print("Error executing public key verify")
         return False
 
+
 if __name__ == '__main__':
-    pr,pu = generate_keys()
+    pr, pu = generate_keys()
     message = b"This is a secret message"
     signature = sign(message, pr)
     correct = verify(message, signature, pu)
@@ -38,11 +54,9 @@ if __name__ == '__main__':
     else:
         print("Bad signature")
 
-    #todo:simulate attacks
-    pr_fake,pu_fake = generate_keys()
-    fake_test = verify(message,signature,pu_fake)
+    # todo:simulate attacks
+    pr_fake, pu_fake = generate_keys()
+    fake_test = verify(message, signature, pu_fake)
     print(f"fake test is: {fake_test}")
 
     print(pu.public_numbers())
-
-
--- TxBlock.py
+++ TxBlock.py
@@ -2,8 +2,6 @@ from BasicBlockChain import CBlock
 import Signitures
 from Transaction import TX
 import pickle
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
 
 
 class TxBlock(CBlock):
@@ -27,49 +25,16 @@ if __name__ == "__main__":
         pr2, pu2 = Signitures.generate_keys()
         pr3, pu3 = Signitures.generate_keys()
 
-
-        def serialize_pr_key(private_key, message: str):
-            pm = private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                           format=serialization.PrivateFormat.PKCS8,
-                                           encryption_algorithm=serialization.BestAvailableEncryption(
-                                               f"{bytes(message)}"))
-            return pm.splitlines()[0]
-
-
-        def serialize_pr_key(private_key, message:str):
-            bmessage = bytes(message,'utf-8')
-            pm = private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                              format=serialization.PrivateFormat.PKCS8,
-                              encryption_algorithm=serialization.BestAvailableEncryption(bmessage))
-            print(f"private key: {pm.splitlines()[0]}")
-            return pm.splitlines()[0]
-
-        def serialize_pu_key(public_key):
-            pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
-              format=serialization.PublicFormat.SubjectPublicKeyInfo)
-            return pem.splitlines()[0]
-
-
         def load_serialized_key(data_file):
              with open(data_file, 'rb') as loadfile:
-                   loaded_pu = pickle.load(loadfile)
-                   serialized_pu = serialization.load_pem_public_key(loaded_pu, backend=default_backend())
-                   print(Signitures.verify(message,sig,pu1))
-             return serialized_pu
-
-
-        def serialize_pu_key(public_key):
-            pem = public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo)
-            print(f"public key {pem.splitlines()[0]}")
-            return pem.splitlines()[0]
+                   ser_pu = pickle.load(loadfile)
+                   return ser_pu
 
 
         # creating some transactions
         Tx1 = TX()
-        Tx1.add_input(serialize_pu_key(pu1), 3)
-        Tx1.add_output(serialize_pu_key(pu2), 5)
+        Tx1.add_input(pu1, 3)
+        Tx1.add_output(pu2, 5)
         message = b"simple message"
         # Tx1.sign(serialize_pr_key(pr1, message))
         print(Tx1.is_valid())
@@ -80,33 +45,30 @@ if __name__ == "__main__":
         save_file = open('save.dat', 'wb')
         pickle.dump(Tx1, save_file)
 
+        Tx1 = TX()
+        Tx1.add_input(pu1, 3)
+        Tx1.add_output(pu2, 5)
+        Tx1.sign(pr1)
+        print(Tx1.is_valid())
+
+        message = b"some message"
+        sig = Signitures.sign(message,private_key=pr1)
+
+        savefile = open('save.dat','wb')
+        pickle.dump(Tx1,savefile)
+
+        savefile.close()
+
+        loadfile = open("save.dat", "rb")
+        newTx = pickle.load(loadfile)
+        print(newTx.is_valid())
+
+        save_file.close()
+
+        # read the file again as binary read using pickle module
+        loaded_pu = load_serialized_key("save.dat")
+        print(loaded_pu)
+
     except Exception as e:
-        print(e)
-
-
-
-    Tx1 = TX()
-    Tx1.add_input(serialize_pu_key(pu1), 3)
-    Tx1.add_output(serialize_pu_key(pu2), 5)
-    Tx1.sign(pr1)
-    print(Tx1.is_valid())
-
-    message = b"some message"
-    sig = Signitures.sign(message,private_key=pr1)
-
-    savefile = open('save.dat','wb')
-    pickle.dump(Tx1,savefile)
-
-    savefile.close()
-
-    loadfile = open("save.dat", "rb")
-    newTx = pickle.load(loadfile)
-    print(newTx.is_valid())
-
-
-    save_file.close()
-
-    # read the file again as binary read using pickle module
-    loaded_pu = load_serialized_key("save.dat")
-    print(loaded_pu)
-
+            print(f"Exception has occured: {e}")
+            print(e.__traceback__.tb_lineno)
