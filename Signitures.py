e1327fa778ce68cd29211b6e42fec450e12261f2
payam49er@gmail.com <payam49er@gmail.com>
2021-08-20 08:23:28 -0400

    fixed a bug for serialization and deserialization of public key

--- Signitures.py
+++ Signitures.py
@@ -3,16 +3,21 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography import exceptions
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidSignature
 
 
 # returning a serialized public key and the raw private key for now
 def generate_keys():
+    '''
+     Serialized public key. For now, private key is not serialized. We will do that later
+    '''
     private = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = private.public_key()
-    pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+    pu = public_key.public_bytes(encoding=serialization.Encoding.PEM,
                                   format=serialization.PublicFormat.SubjectPublicKeyInfo)
-    print(f"public key {pem.splitlines()[0]}")
-    return private, pem.splitlines()[0]
+    print(f"public key {pu.splitlines()[0]}")
+    return private, pu
 
 
 # in a way, signing is like decrypting a message
@@ -30,7 +35,10 @@ def serialize_pr_key(private_key, message: str):
     return pm.splitlines()[0]
 
 
-def verify(message, signature, public_key):
+def old_verify(message, signature, public_key):
+    '''
+    At this moment, public key is serialized. We need the following logic to work with serialized key
+    '''
     try:
         message = bytes(str(message), 'utf-8')
         public_key.verify(signature, message, padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
@@ -41,6 +49,28 @@ def verify(message, signature, public_key):
     except:
         print("Error executing public key verify")
         return False
+
+
+def verify(message, signiture, public_ser_key):
+    public = serialization.load_pem_public_key(public_ser_key,backend=default_backend())
+    message = bytes(str(message),'utf-8')
+    try:
+        public.verify(signiture,message,
+                      padding.PSS(
+                          mgf=padding.MGF1(hashes.SHA256()),
+                          salt_length=padding.PSS.MAX_LENGTH),
+                      hashes.SHA256())
+        return True
+    except exceptions.InvalidSignature:
+        return False
+    except:
+        print("Error executing public key verify")
+        return False
+
+
+
+
+
 
 
 if __name__ == '__main__':
